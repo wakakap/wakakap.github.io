4fa1427966b7dccbc6f24609433fa3ffeea78b98
wakakap <chen644280147@gmail.com>
2023-03-31 06:17:54 +0800

    add Kirbybeats

---
+++ js/play_daybreak_frontline.js
@@ -0,0 +1,74 @@
+var i = 0;
+var songtime = 0;
+var intervalId;
+var canvas;
+var context;
+var currentFrame = 0;
+
+var animationFrames = [
+	'frame_02_delay-0.04s.png',
+	'frame_04_delay-0.04s.png',
+	'frame_06_delay-0.04s.png',
+	'frame_08_delay-0.04s.png',
+	'frame_10_delay-0.04s.png',
+	'frame_12_delay-0.04s.png',
+	'frame_14_delay-0.04s.png',
+	'frame_16_delay-0.04s.png',
+];
+
+
+function play(){
+	var audio = document.getElementById("myAudio");
+	audio.play();
+	setTimeInfo();
+	intervalId = setInterval(draw, 58);
+	setInterval(updateTime, 5);
+}
+
+function pause(){
+	var audio = document.getElementById("myAudio");
+	audio.pause();
+	clearInterval(intervalId);
+	clearInterval(updateTime);
+}
+
+function updateTime(){
+	songtime = Math.floor(document.getElementById("myAudio").currentTime);
+	if (i < timeInfo.length && songtime >= timeInfo[i]) {
+		currentFrame = 0;
+		i++;
+	}
+}
+
+function setTimeInfo() {
+	var txtFile = new XMLHttpRequest();
+	txtFile.open("GET", "../media/daybreak frontline_time_info.txt", true);
+	txtFile.onreadystatechange = function() {
+		if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+			if (txtFile.status === 200) {  // Makes sure it's found the file.
+				console.log(txtFile.status);
+				var allText = txtFile.responseText;
+				timeInfo = allText.trim().split('\n').map(function(x) { return parseInt(x); });
+				document.getElementById("timeInfo").innerHTML = "<pre>" + allText + "</pre><pre>" + JSON.stringify(timeInfo) + "</pre>"; // 输出时间信息到页面中
+			}
+		}
+	}
+	txtFile.send(null);
+}
+
+function draw(){
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");
+	if (currentFrame >= animationFrames.length) {
+		
+		// clearInterval(intervalId);
+	} else {
+		var image = new Image();
+		image.onload = function(){
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.drawImage(image, 0, 0);
+			currentFrame++;
+		};
+		image.src = `../media/Kirbybeats/`+animationFrames[currentFrame];
+	}
+}
---
+++ python/musicbpm.py
@@ -0,0 +1,19 @@
+import librosa
+import numpy as np
+
+# 加载音频文件
+audio_file = 'DAYBREAK FRONTLINE.aac'
+y, sr = librosa.load(audio_file)
+
+# 提取音频特征
+tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+
+# 将帧编号转换为时间轴上的时间
+times = librosa.frames_to_time(beats, sr=sr)
+
+# 保存节拍时间信息到文本文件
+np.savetxt('beat_times.txt', times, fmt='%.8f')
+
+# 输出节拍数量和时间范围
+print('Number of beats:', len(beats))
+print('Time range:', times[-1] - times[0])
