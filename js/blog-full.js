d9e0704f9ee0d84116bfb6aedbe3ee78fe8e96a3
wakakap <chen644280147@gmail.com>
2023-04-11 15:13:49 +0800

    diaryupdate

--- js/blog-full.js
+++ js/blog-full.js
@@ -1,59 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // 创建Showdown对象
   const converter = new showdown.Converter();
   const fileNames = [];
 
-  // 读取文件名列表
-  fetch('../markdown/diary/filenames.txt')
+  fetch('../markdown/diary/0000-filenames.txt')
     .then(response => response.text())
     .then(data => {
-      // 将文件名用逗号分隔，并添加到fileNames数组中
-      const names = data.split(',');
+      const names = data.split(',').map(name => name.trim());//也许有空格。但已经修正，可注销
       fileNames.push(...names);
-
-      // 排序文件名，倒序
-      fileNames.sort().reverse();
-
-      // 取前三个文件名加载
-      for (let i = 0; i < fileNames.length; i++) {
+      // fileNames.sort().reverse();//倒序排列 前期处理已经排序，这里不需要再写了，也许可以加快速度
+      for (let i = 0; i < fileNames.length; i++) {//限制加载日记数量
         loadMarkdown(fileNames[i], converter);
       }
     });
-
-  function loadMarkdown(filename, converter) {
-    // 使用fetch方法异步加载Markdown文件
-    fetch(`../markdown/diary/${filename}.md`)
-      .then(response => response.text())
-      .then(markdownContent => {
-        // 将Markdown内容转换为HTML
-        const htmlContent = converter.makeHtml(markdownContent);
-
-        // 从文件名中提取无日期纯标题
-        const segfilename = filename.substring(0, filename.length).split('-');
-        const title = segfilename[segfilename.length - 1];
-        const date = segfilename.slice(0, segfilename.length - 1).join('-');
-        // 创建新的list item元素，并将标题和HTML内容添加到其中
-        const listItem = document.createElement('li');
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = title;
-        const subtitleElement = document.createElement('span');
-        subtitleElement.textContent = date;
-        subtitleElement.classList.add('subtitle');
-        titleElement.appendChild(subtitleElement);
-        const contentElement = document.createElement('div');
-        contentElement.innerHTML = htmlContent;
-        listItem.appendChild(titleElement);
-        listItem.appendChild(contentElement);
-
-        // 将新的list item元素添加到列表中
-        const markdownList = document.querySelector('#markdown-list');
-        markdownList.appendChild(listItem);
-
-        // 对列表进行排序，按照标题倒序
-        const lis = Array.from(markdownList.getElementsByTagName('li'));
-        lis.sort((a, b) => {
-          return b.querySelector('h2').textContent.localeCompare(a.querySelector('h2').textContent)
-        }).forEach(li => markdownList.appendChild(li));
-      });
-  }
 });
+
+
+async function loadMarkdown(filename, converter) {
+  const markdownContent = await fetch(`../markdown/diary/${filename}.md`).then(response => response.text());
+
+  const segments = filename.split('-');
+  const date = `${segments[0]}-${segments[1]}-${segments[2]}`;
+  const title = segments.slice(3).join('-');
+  const li = document.createElement('li');
+
+  // create button to toggle content visibility
+  const button = document.createElement('button');
+  button.innerText = '展开';
+  button.addEventListener('click', () => {
+    if (div.hidden) {
+      div.hidden = false;
+      button.innerText = '收起';
+    } else {
+      div.hidden = true;
+      button.innerText = '展开';
+    }
+  });
+
+  // create div to contain markdown content
+  const div = document.createElement('div');
+  div.innerHTML = converter.makeHtml(markdownContent);
+  div.hidden = true;//可以将代码中的div.style.display = 'none'改成div.hidden = true。这样在一开始就不会加载所有内容，而是当点击展开按钮时才显示相应的内容。
+
+  // append title and button to li element
+  const h2 = document.createElement('h2');
+  h2.innerHTML = `${title}<span class="subtitle"> date：${date} </span>`;
+  h2.appendChild(button);
+  li.appendChild(h2);
+
+  // append div to li element
+  li.appendChild(div);
+
+  const markdownList = document.querySelector('#markdown-list');
+  markdownList.appendChild(li);
+}
---
+++ markdown/diary/0000-生成txt.py
@@ -0,0 +1,22 @@
+import os
+import glob
+import datetime
+
+# 获取当前日期
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+
+# 找到所有扩展名为md的文件
+files = glob.glob("*.md")
+
+# 去掉文件名中的扩展名
+file_names = [os.path.splitext(f)[0] for f in files]
+
+# 将文件名列表反转
+file_names.reverse()
+
+# 将文件名按逗号分隔并拼接成字符串
+file_names_str = ",".join(file_names)
+
+# 创建新文件并将文件名写入其中
+with open(f"0000-filenames_{today}.txt", "w") as f:
+    f.write(file_names_str)
